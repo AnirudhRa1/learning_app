2d3ea6215c5743282ce0133c4a0f7399be0fa103
Anirudh <surekhasurekha432@gmail.com>
2025-07-23 15:08:06 +0530

    fixing backend

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 
 
 // port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on ${PORT}`);
